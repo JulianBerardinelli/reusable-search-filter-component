1a2a58aa7791108dee7b543ff897cf67ecc00a35
Julián Berardinelli <jberardinelli@MacBook-Pro-de-Julian.local>
2024-10-22 11:13:39 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'the-guitar-hub_1',
+  webDir: 'www'
+};
+
+export default config;
